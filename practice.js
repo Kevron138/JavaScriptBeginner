fc66bbadb8e8c0ca41ac9d9367b4ddcaf573b727
Kevron138 <104953842+Kevron138@users.noreply.github.com>
2022-05-10 20:00:37 -0400

    edit types

---
+++ practice.js
@@ -0,0 +1,19 @@
+//declare variable
+
+//depricated, and is not considered relevant at this time.
+//ar kevin = "kevin is dumb and forget everything already";
+
+//not a consistent or having a fixed pattern ; liable to change.
+
+//declare a variable that you intend to reassign later
+//let kevin = "seriously how did he forget all of thise...";
+
+//const kevin = "is a cool guy";
+
+let kevin = "I have theh memory of a mouse"
+
+let num = 100
+
+let bull = false
+
+console.log(kevin)
